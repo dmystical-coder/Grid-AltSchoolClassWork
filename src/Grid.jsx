a9cad503049abebb6c05e30ad0ecac1a74226184
Abdulsalam Baruwa <dmystical.coder@gmail.com>
2023-09-06 17:01:42 +0000

    Initial attempt

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -1,7 +1,27 @@
 import { range } from './utils';
 
 function Grid({ numRows, numCols }) {
-  return <div className="grid">{/* TODO */}</div>;
+  const cards = [];
+
+  const rowsRange = range(numRows);
+  const colsRange = range(numCols);
+
+  function cols() {
+    for (let col=0; col<colsRange; col++) {
+      let column = (<div><p>Paragraph</p></div>);
+      cards.push(column);
+      
+    }
+    
+    return {cards};
+  }
+
+  
+
+
+
+
+  return <div className="grid">{cards}</div>;
 }
 
 export default Grid;
--- src/utils.js
+++ src/utils.js
@@ -4,10 +4,16 @@ export const range = (start, end, step = 1) => {
 
   // handle scenario where end is not specified range(5) - [0, 1, 2, 3, 4]
   // a conditional check for end
+  if (end === undefined || end === null) {
+    end = start;
+    start = 0;
+  }
 
   for (let i = start; i < end; i += step) {
     // loop code
+    output.push(i);
   }
 
   return output;
 };
+
